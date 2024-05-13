b35a9b1d392319533aa1093483b3d8cc08d02832
iker-gayangos-tfg <ikergayangostfg@gmail.com>
2024-05-13 20:26:56 +0200

    (API): Obtener ulr de departamentos

--- WebScrapingAPI/WebScrapingAPI/Controllers/WebScrapingController.cs
+++ WebScrapingAPI/WebScrapingAPI/Controllers/WebScrapingController.cs
@@ -1,43 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Linq;
+using System.Security.Policy;
+using WebScrapingAPI.Models;
+using static WebScrapingAPI.Controllers.WebScrapingController;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebScrapingAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class WebScrapingController : ControllerBase
     {
-        // GET: api/<WebScrapingController>
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> Scraping()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                IWebDriver driver = new ChromeDriver();
+
+                List<string> urlDepartamentos = new List<string>();
+
+
+                //Obtener url de departamentos
+                driver.Navigate().GoToUrl("https://investigacion.ubu.es/investigadores");
+
+                var divFacultades = driver.FindElements(By.CssSelector(".investigadores-explorar__category-content")).FirstOrDefault(x => x.Text.Contains("Facultades y Centros de investigación"));
+                if (divFacultades != null)
+                {
+                    var facultades = divFacultades.FindElement(By.TagName("ul")).FindElements(By.TagName("li"));
+                    foreach (var facultad in facultades)
+                    {
+                        urlDepartamentos.Add(facultad.FindElement(By.TagName("a")).GetAttribute("href").ToString());
+                    }
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        // GET api/<WebScrapingController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<WebScrapingController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<WebScrapingController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<WebScrapingController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }

fe1e6d3e788f16cebc20061685b974169253a295
iker-gayangos-tfg <ikergayangostfg@gmail.com>
2024-05-14 03:02:59 +0200

    (API): Creacion DBContext. Modelo de departamentos.

---
+++ WebScrapingAPI/WebScrapingAPI/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WebScrapingAPI.Models;
+
+namespace WebScrapingAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { 
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Investigador>(entity =>
+            {
+                entity.HasOne(d => d.Departamento)
+                    .WithMany(p => p.Investigadores)
+                    .HasForeignKey(d => d.FoDepartamento)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("[FK_Investigador_Departamento]");
+            });
+        }
+    }
+}
---
+++ WebScrapingAPI/WebScrapingAPI/Models/Departamento.cs
@@ -0,0 +1,14 @@
+﻿namespace WebScrapingAPI.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Url { get; set; }
+
+        public ICollection<Investigador>? Investigadores { get; set; }
+
+    }
+}

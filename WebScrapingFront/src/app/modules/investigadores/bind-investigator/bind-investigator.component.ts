e256a113a355f35f4fd0ac8e93af00e7f74813da
iker-gayangos-tfg <ikergayangostfg@gmail.com>
2024-06-04 03:11:10 +0200

    (Front): Modal para realizar la fusion de investigadores

---
+++ WebScrapingFront/src/app/modules/investigadores/bind-investigator/bind-investigator.component.ts
@@ -0,0 +1,82 @@
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { WebScrapingService } from '../../../services/web-scraping.service';
+import { SnackbarNotifyService } from '../../snackbar-notify/snackbar-notify.service';
+
+@Component({
+  selector: 'bind-investigator',
+  templateUrl: './bind-investigator.component.html',
+  styleUrls: ['./bind-investigator.component.scss']
+})
+export class BindInvestigatorComponent {
+
+  btnDisabled = true;
+
+  public selection: SelectionModel<any>;
+
+  public tableColumns: string[] = ['select', 'code', 'description', 'manufacturer', 'manufacturerCode', 'technicalLocationName'];
+
+  investigators: any[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<BindInvestigatorComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private webScrapingService: WebScrapingService,
+    private snackbar: SnackbarNotifyService,
+  ) {
+    this.investigators = data;
+    const initialSelection: any[] = [];
+    const allowMultiSelect = false;
+    this.selection = new SelectionModel<any>(allowMultiSelect, initialSelection);
+  }
+  
+
+  ngOnInit(): void {
+    this.selection.changed.subscribe(
+      () => {
+        this.validateFields();
+      }
+    )
+  }
+
+  validateFields () {
+    if(this.selection.selected.length == 1) {
+      this.btnDisabled = false;
+    } else {
+      this.btnDisabled = true;
+    }
+  }
+
+  public close(dialogResult?: any) {
+    this.dialogRef.close(dialogResult);
+  }
+
+  public onSubmit() {
+    var idInvestigatorSelected = this.selection.selected[0].id;
+    var idsInvestigatorsToBind: any[] = [];
+    this.investigators.forEach(
+      element => {
+        if(idInvestigatorSelected != element.id){
+          idsInvestigatorsToBind.push(element.id)
+        }
+      }
+    );
+    this.webScrapingService.bindInvestigators({
+      Id: idInvestigatorSelected,
+      InvestigatorIds: idsInvestigatorsToBind
+    }).subscribe(
+      () => {
+        this.snackbar.openSnackBar('Investigadores fusionados correctamente', 'sb-success');
+        this.closeModal()
+      },
+      () => {
+        this.snackbar.openSnackBar('Los investigadores no se han podido fusionadar', 'sb-error');
+      }
+    )
+  }
+
+  closeModal() {
+    this.dialogRef.close()
+  }
+}

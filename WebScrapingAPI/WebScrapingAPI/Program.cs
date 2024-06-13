334e581287d0a261b7b66cb99c73ad53bb3c1aae
iker-gayangos-tfg <ikergayangostfg@gmail.com>
2024-06-13 22:13:08 +0200

    (API): Configuracion de entorno para correcta conexion con front

--- WebScrapingAPI/WebScrapingAPI/Program.cs
+++ WebScrapingAPI/WebScrapingAPI/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebScrapingAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer("Server=desktop-nnliiqk\\sqlexpress;Database=WebScrapingDev;Trusted_Connection=True;MultipleActiveResultSets=True;Trust Server Certificate=true")
+);
+builder.Services.AddCors(options => options.AddPolicy("AllowWebApp",
+    builder => builder.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+)); 
 
 var app = builder.Build();
 
@@ -15,6 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowWebApp");
 
 app.UseHttpsRedirection();
 
